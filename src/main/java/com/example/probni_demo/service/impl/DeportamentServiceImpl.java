e588a43feb187ae60366701468b010ff9b6dc2dd
Дарова <yulechka140200@gmail.com>
2023-10-16 20:43:29 +0500

    4

---
+++ src/main/java/com/example/probni_demo/service/impl/DeportamentServiceImpl.java
@@ -0,0 +1,93 @@
+package com.example.probni_demo.service.impl;
+
+import com.example.probni_demo.domain.Employee;
+import com.example.probni_demo.service.EmployeeSelaryService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DeportamentServiceImpl
+//        implements EmployeeSelaryService
+{
+
+//    private final List<Employee> deportament = new ArrayList<>();
+//
+//@Override
+//    public List<Integer> maxSalary(Integer departmentId){
+//
+//        Map<Integer, Integer> deportament = new HashMap<>();
+//
+//        Employee employee = null;
+//        int maxSalary = Integer.MIN_VALUE;
+//        for (Employee emp: deportament) {
+//            if (emp.getSelary() > maxSalary) {
+//                maxSalary = emp.getSelary();
+//                employee = emp;
+//            }
+//        }
+//        return departmentId;
+//    }
+//
+//    @Override
+//    public Map<Integer,Integer> minSalary(Integer departmentId) {
+//
+//    return departmentId;
+//    }
+//
+//    @Override
+//    public Map<Integer,Integer> allSalary(Integer departmentId) {
+//
+//
+//    }
+//
+//    @Override
+//    public Map<Integer,Integer> all(){
+//
+//        for (Map.Entry<Integer, Integer> contact: deportament.entrySet()) {
+//            System.out.println("Контакт " + contact.getKey() + ": " + contact.getValue());
+//        }
+//    }
+//
+//
+//    private static void printInfoAboutEmployees() {
+//        for (Employee employee: EMPLOYEES) {
+//            System.out.println(employee);
+//        }
+//    }
+//
+//    private static int calculateTotalSalaries() {
+//        int sum = 0;
+//        for (Employee employee: EMPLOYEES) {
+//            sum += employee.getSelary();
+//        }
+//        return sum;
+//    }
+//
+//    private static Employee findEmployeeWitMinSalary() {
+//        Employee employee = null;
+//        int minSalary = Integer.MAX_VALUE;
+//        for (Employee emp: EMPLOYEES) {
+//            if (emp.getSelary() < minSalary) {
+//                minSalary = emp.getSelary();
+//                employee = emp;
+//            }
+//        }
+//        return employee;
+//    }
+//
+//
+//    private static double findAverageSalary() {
+//        return  calculateTotalSalaries() / (double) EMPLOYEES.length;
+//    }
+//
+//    private static void printFullNameOfEmployees() {
+//        for (Employee employee: EMPLOYEES) {
+//            System.out.println(employee.getFullName());
+//        }
+//    }
+
+}
